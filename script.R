846087989824beb24406a88b666d11d95b370526
FagnerOnesko <fagneronesko@alunos.utfpr.edu.br>
2019-10-16 23:43:43 -0300

    Add distancia euclidiana

--- script.R
+++ script.R
@@ -8,6 +8,19 @@ for(col in 1:4){
   }
 }
 
+#dividir dataset em treinamento e teste
 treinamento <- irisNorm[c(1:40,51:90, 101:140),]
 teste <-irisNorm[c(41:50,91:100, 141:150),]
+
+#distancia euclidiana
+matrizDist <- matrix(nrow=30,ncol=120);
+
+for(linTeste in 1:30){
+  for (linTreino in 1:120) {
+    matrizDist[linTeste, linTreino] <- sqrt((teste[linTeste,1]-treinamento[linTreino,1])^2 + 
+                                              (teste[linTeste,2]-treinamento[linTreino,2])^2 +
+                                              (teste[linTeste,3]-treinamento[linTreino,3])^2 + 
+                                              (teste[linTeste,4]-treinamento[linTreino,4])^2)
+  }
+}
 
